3773d4af41808ef6aa5409fcca76a167f9accb9c
Pradeep Khatri <pradeepkhatri44@live.com>
2025-04-26 17:23:09 +0530

    commit learning React
    commit learning React

--- ReactPK/src/App.jsx
+++ ReactPK/src/App.jsx
@@ -22,24 +22,51 @@ const App = () => {
     setA(25)
   }
 
-  return <div>
-    <h1>Hello {PK}, you are {age} years old. You reside in {place}.</h1>
-    <button onClick={abc}>Change User</button>
+  const submitHandler = (e) =>{
+    e.preventDefault()
+    console.log(username)
+    setUsername('')
+  }
 
-    <h1>Value of a is {a}</h1>
-    <button onClick={changeA}>Change A</button>
+  const [username, setUsername] = useState('')
 
+  return (
     <div>
-      <h3>Number is {num}</h3>
-      <button onClick={()=>setNum(num+35)}>Increment</button>
-      <button onClick={()=>setNum(num-35)}>Decrement</button>
-    </div>
+      <h1>
+        Hello {PK}, you are {age} years old. You reside in {place}.
+      </h1>
+      <button onClick={abc}>Change User</button>
 
-    <div>
-      <h2 className="text-5xl">Hello Guys</h2>
-      <h2>Hello</h2>
+      <h1>Value of a is {a}</h1>
+      <button onClick={changeA}>Change A</button>
+
+      <div>
+        <h3>Number is {num}</h3>
+        <button onClick={() => setNum(num + 35)}>Increment</button>
+        <button onClick={() => setNum(num - 35)}>Decrement</button>
+      </div>
+
+      <div>
+        <h2 className="text-5xl bg-green-700 text-black">Hello Guys</h2>
+      </div>
+
+      <form onSubmit={(e)=>{
+        submitHandler(e)
+      }}>
+        <input
+          className="px-4 py-3 rounded text-xl m-5"
+          type="text"
+          placeholder="Enter your name:"
+          onChange={(e)=>{
+            setUsername(e.target.value)
+          }}
+          value={username}
+        />
+
+        <button className="px-4 text-white py-3 m-5 text-xl font-semibold bg-emerald-300 rounded">Submit</button>
+      </form>
     </div>
-  </div>
+  );
 }
 
 export default App
